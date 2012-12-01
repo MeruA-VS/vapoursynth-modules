3929a2a696a1bf12c40eb8d1434f02477d52f6db
Daniel <surukko@gmail.com>
2012-12-01 18:31:31 +0100

    fixed a typo, some cosmetic changes

--- supersampledantialiasing.py
+++ supersampledantialiasing.py
@@ -1,14 +1,14 @@
-# supersampledantialiasing.py (2012-11-25)
+# supersampledantialiasing.py (2012-12-01)
 # requirements: mt_masktools.dll(avs)
 #               SangNom.dll(avs)
 #               fmtconv.dll(vs)
-#               RemoveGrain.dll(avs)(opt:sharpen)
-#               Repair.dll(avs)(opt:sharpen)
+#               RemoveGrain.dll(avs)
+#               Repair.dll(avs)
 
 import vapoursynth as vs
 
 def clamp(minimum, x, maximum):
-    return int(max(minimum, min(round(x), maximum)))
+	return int(max(minimum, min(round(x), maximum)))
 
 class SupersampledAntialiasing(object):
 	def __init__(self, core):
@@ -38,14 +38,14 @@ class SupersampledAntialiasing(object):
 	def mt_makediff(self, c1, c2, planes=0):
 		expr = lambda x, y: x - y + self.mid
 		return self.mt_lutxy(c1, c2, expr, planes)
-
+	
 	def ssaa_BuildMask(self, clip, edgelvl=11, radius=2, thY=255, thC=255, mode='rectangle'):
 		srad = radius-1
 		
 		m = self.edge(clip, 'prewitt', edgelvl, edgelvl, edgelvl, edgelvl, chroma='copy')
 		
 		if srad > 0:
-			if ((mode == 'losange') or ((mode == 'ellipse') and ((srad % 3) != 1))):
+			if mode == 'losange' or (mode == 'ellipse' and ((srad % 3) != 1)):
 				mode_m = 'both'
 			else:
 				mode_m = 'square'
@@ -64,7 +64,7 @@ class SupersampledAntialiasing(object):
 		fh   = clip.height
 		
 		if clip.format.id != vs.YUV420P8:
-			self.bitdepth(clip=clip, csp=vs.YUV420P8)
+			clip = self.bitdepth(clip=clip, csp=vs.YUV420P8)
 		
 		mask = self.ssaa_BuildMask(clip=clip, edgelvl=th_mask)
 		
@@ -74,8 +74,8 @@ class SupersampledAntialiasing(object):
 		aac  = self.bitdepth(clip=self.resample(aac, fw, fh, kernel='blackman'), csp=vs.YUV420P8)
 		
 		if sharpen == True:
-			self.max = 2 ** clip.format.bits_per_sample - 1
-			self.mid = self.max // 2 + 1
+			self.max       = 2 ** clip.format.bits_per_sample - 1
+			self.mid       = self.max // 2 + 1
 			self.lut_range = range(self.max + 1)
 			aaD   = self.mt_makediff(clip, aac)
 			shrpD = self.mt_makediff(aac, self.rgrain(aac, 20))
@@ -91,10 +91,12 @@ class SupersampledAntialiasing(object):
 		usage = '''
 		Small antialiasing function for 1080p.
 		
+		Input must be vs.YUV420P8, if not, it will be automatically converted.
+
 		ssaa(clip, th_mask=11, sharpen=False, smask=False)
 			th_mask: The lower the threshold, more pixels
 			         will be antialiased.
-			sharpen: performs contra-sharpening on the antialiased zones.
-			smask:   shows the zones antialiasing will be applied.
+			sharpen: Performs contra-sharpening on the antialiased zones.
+			smask:   Shows the zones antialiasing will be applied.
 		'''
 		return usage
