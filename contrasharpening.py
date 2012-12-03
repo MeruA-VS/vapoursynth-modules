e0caad06c2ebb44075b2990bbad33a0180119350
Daniel <surukko@gmail.com>
2012-12-03 12:13:16 +0100

    Small speed up

--- contrasharpening.py
+++ contrasharpening.py
@@ -1,5 +1,6 @@
-# contrasharpening.py (2012-12-01)
+# contrasharpening.py (2012-12-03)
 # Dependencies: RemoveGrain.dll (avs)
+#		RemoveGrainHD.dll (avs)
 #		Repair.dll (avs)
 
 import vapoursynth as vs
@@ -12,6 +13,7 @@ class ContraSharpening(object):
 		self.std       = core.std
 		self.repair    = core.avs.Repair
 		self.rgrain    = core.avs.RemoveGrain
+		self.quantile  = core.avs.Quantile
 		self.lut_range = None
 		self.max       = 0
 		self.mid       = 0
@@ -31,17 +33,30 @@ class ContraSharpening(object):
 		expr = lambda x, y: x - y + self.mid
 		return self.mt_lutxy(c1, c2, expr, planes)
 	
-	def minblur(self, clip):
-		self.max       = 2 ** clip.format.bits_per_sample - 1
+	def minblur(self, clp, r=1, planes=0):
+		if planes is not 0:
+			rg4, rg11, rg20, uvm2, uvm3 = 4, 11, 20, 2, 3
+		else:
+			rg4 = rg11 = rg20 = uvm2 = uvm3 = -1
+		
+		self.max       = 2 ** clp.format.bits_per_sample - 1
 		self.mid       = self.max // 2 + 1
 		self.lut_range = range(self.max + 1)
 		
-		rg11D = self.mt_makediff(clip, self.rgrain(clip, 11, -1))
-		rg4D  = self.mt_makediff(clip, self.rgrain(clip, 4, -1))
-		expr  = lambda x, y: self.mid if ((x - self.mid) * (y - self.mid)) < 0 else (x if abs(x - self.mid) < abs(y - self.mid) else y)
-		DD    = self.mt_lutxy(rg11D, rg4D, expr)
+		if r == 1:
+			RG11D = self.mt_makediff(clp, self.rgrain(clp, 11, rg11), planes=planes)
+			RG4D  = self.mt_makediff(clp, self.rgrain(clp, 4, rg4), planes=planes)
+		elif r == 2:
+			RG11D = self.mt_makediff(clp, self.rgrain(self.rgrain(clp, 11, rg11), 20, rg20), planes=planes)
+			RG4D  = self.mt_makediff(clp, self.quantile(clp, radius_y=2, radius_u=uvm2, radius_v=uvm2), planes=planes)
+		else:
+			RG11D = self.mt_makediff(clp, self.rgrain(self.rgrain(self.rgrain(clp, 11, rg11), 20, rg20), 20, rg20), planes=planes)
+			RG4D  = self.mt_makediff(clp, self.quantile(clp, radius_y=3, radius_u=uvm3, radius_v=uvm3), planes=planes)
 		
-		return self.mt_makediff(clip, DD)
+		expr = lambda x, y: self.mid if ((x - self.mid) * (y - self.mid)) < 0 else (x if abs(x - self.mid) < abs(y - self.mid) else y)
+		DD   = self.mt_lutxy(RG11D, RG4D, expr, planes=planes)
+		
+		return self.mt_makediff(clp, DD, planes=planes)
 		
 	def contrasharpening(self, filtered, original):
 		self.max       = 2 ** filtered.format.bits_per_sample - 1
@@ -55,3 +70,14 @@ class ContraSharpening(object):
 		ssDD = self.mt_lutxy(ssDD, ssD, lambda x, y: x if abs(x - self.mid) < abs(y - self.mid) else y)
 		
 		return self.mt_adddiff(filtered, ssDD)
+	
+	def usage(self):
+		usage='''
+		ContraSharpening module ported from:
+		http://forum.doom9.org/showpost.php?p=1474191&postcount=337
+		
+		contrasharpening(filtered, original)
+			filtered: A clip blured due the filtering done on it.
+			original: The same clip before the filtering.
+		'''
+		return usage
